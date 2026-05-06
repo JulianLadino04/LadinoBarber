2046ba1137236d964c5b29485d5ec0ad3eccaf12
Julian Ladino <147188241+JulianLadino04@users.noreply.github.com>
2026-05-06 14:22:31 -0500

    Proyecto inicial backend Ladino Barber

---
+++ src/main/java/com/ladinobarber/LadinoBarberApplication.java
@@ -0,0 +1,13 @@
+package com.ladinobarber;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LadinoBarberApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LadinoBarberApplication.class, args);
+	}
+
+}
